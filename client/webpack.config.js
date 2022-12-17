117c5e1faec7a9adc51d66de48b7fa69c7f643c9
FDeaquino <fidel.deaquino@gmail.com>
2022-12-17 15:53:31 -0600

    imported compiled CSS module

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,32 @@ const path = require("path");
 module.exports = {
     entry: "./src/js/index.js",
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
-    }, 
+    },
     mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
 }
